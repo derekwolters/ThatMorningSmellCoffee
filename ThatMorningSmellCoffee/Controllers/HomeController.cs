ec6120d452d333e219b99d0f24d278b853abc58b
Derek Wolters <derek.wolters@gmail.com>
2017-04-28 16:29:33 -0400

    First commit

---
+++ ThatMorningSmellCoffee/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ThatMorningSmellCoffee.Models;
+
+namespace ThatMorningSmellCoffee.Controllers
+{
+    public class HomeController : Controller
+    {
+        private CoffeeAppDBEntities1 Db = new CoffeeAppDBEntities1();
+        public ActionResult Index()
+        {
+           var list = Db.Items.ToList();
+            return View(list);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "What better combo could there be?";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "It's a trap!";
+
+            return View();
+        }
+    }
+}
